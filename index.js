7d475b5a3a65c512d35595e8b5663f8fa59d0255
이원찬 <twoone14@tukorea.ac.kr>
2024-08-06 00:37:00 +0900

    chore: mysql2 세팅

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 import express from "express";
 import logger from "morgan";
+import mysql from "mysql2/promise";
 // import dotenv from "dotenv";
 
 // dotenv.config();
 
 const app = express();
 app.use(logger("dev"));
+
+const pool = mysql.createPool({
+  host: "localhost",
+  port: 3301,
+  user: "root",
+  password: "1234",
+  database: "express1",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  maxIdle: 10,
+});
+
+let [rows, fields] = await pool.execute("SELECT * FROM user");
+
+console.log(rows, fields);
+
 
 app.get("/", (req, res) => {
   res.send("Express Server Update");
